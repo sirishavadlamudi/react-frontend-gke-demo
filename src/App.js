17d87f09561fa35634a4d455f7a86f3e529743c8
sirishavadlamudi <vsirisha028@gmail.com>
2025-10-28 13:42:15 +0530

    Initial React app setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from React on GKE 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
